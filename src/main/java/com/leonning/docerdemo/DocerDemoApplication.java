eac075dba91d152a5f551892f4aa62d81eb12de4
Leon <job@liaojianing.com>
2021-09-10 14:01:25 +0800

    init

---
+++ src/main/java/com/leonning/docerdemo/DocerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.leonning.docerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocerDemoApplication.class, args);
+    }
+
+}
